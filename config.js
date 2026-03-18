f645011d5a5357f968ee620a4189afcd68d3af3a
competitor-01 <55604744+kantans@users.noreply.github.com>
2026-03-18 17:28:27 +0200

    feat/project-base

---
+++ config.js
@@ -0,0 +1,1 @@
+export const range = [10000, 20000];
---
+++ script.js
@@ -0,0 +1,19 @@
+import {range} from "./config.js";
+
+export function generateNumber() {
+	const [max, min] = range;
+	const numbers = [];
+
+	// šeit veicam ciklu, līdz tiek atrasti 5 randomizēti skaitļi kas dalās ar 2 un 3
+	while(numbers.length < 5) {
+		const generatedNumber = Math.floor(Math.random() * (max - min + 1)) + min
+
+		if((generatedNumber % 3 === 0) && (generatedNumber % 2 === 0)) {
+			numbers.push(generatedNumber);
+		}
+	}
+
+	return numbers;
+}
+
+console.log(generateNumber());
